d70126f7c5d540a968867caf2fa7ad8232939591
vananhx <109174543+vananhx@users.noreply.github.com>
2023-04-20 03:25:21 +0700

    chuong7

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    $viewData=[];
-    $viewData["title"] = "Home Page - Online Store";
-    return view('home.index')->with("viewData", $viewData);
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     $viewData=[];
+//     $viewData["title"] = "Home Page - Online Store";
+//     return view('home.index')->with("viewData", $viewData);
+// });
+Route::get('/','App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('/about','App\Http\Controllers\HomeController@about')->name("home.about");
